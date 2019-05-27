e303d19fc50702b37021ec3b0e38678c248028d9
Alan Anderson <alan.anderson@udacity.com>
2019-05-27 08:59:42 -0700

    Update after CR

--- scale-generator/scale_generator.go
+++ scale-generator/scale_generator.go
@@ -1,29 +1,31 @@
 package scale
 
-import "strings"
-
-var sharps = []string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
-var flats = []string{"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"}
-
-var sharpTonics = []string{"C", "a", "G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#"}
-
-var flatTonics = []string{"F", "Bb", "Eb", "Ab", "Db", "Gb", "d", "g", "c", "f", "bb", "eb"}
+import (
+	"strings"
+)
 
 // Scale returns the scale given a tonic and string of intervals
 func Scale(tonic string, intervals string) []string {
 	var scale []string
-	if indexOf(tonic, sharpTonics) > -1 {
-		scale = sharps
-	} else if indexOf(tonic, flatTonics) > -1 {
-		scale = flats
+	switch tonic {
+	case "C", "a", "G", "D", "A", "E", "B", "F#", "e", "b", "f#", "c#", "g#", "d#":
+		scale = []string{"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"}
+	default:
+		scale = []string{"A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab"}
 	}
-	startingNote := strings.Title(tonic)
 	if intervals == "" {
 		intervals = "mmmmmmmmmmmm"
 	}
 	out := make([]string, len(intervals))
-	out[0] = startingNote
-	curIndex := indexOf(startingNote, scale)
+	out[0] = strings.Title(tonic)
+
+	curIndex := 0
+	for i, n := range scale {
+		if n == out[0] {
+			curIndex = i
+			break
+		}
+	}
 	scaleLength := len(scale)
 	for i, rune := range intervals[0 : len(intervals)-1] {
 		if rune == 'm' {
@@ -36,13 +38,4 @@ func Scale(tonic string, intervals string) []string {
 		out[i+1] = scale[curIndex]
 	}
 	return out
-}
-
-func indexOf(needle string, haystack []string) int {
-	for i, n := range haystack {
-		if n == needle {
-			return i
-		}
-	}
-	return -1
 }
