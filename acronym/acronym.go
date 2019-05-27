8f926b222f5ef643d7dd21545c09d63067505191
Alan Anderson <alan.anderson@udacity.com>
2019-05-26 20:21:58 -0700

    Updated acronym code

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -4,18 +4,21 @@ package acronym
 import (
 	"strings"
 	"unicode"
-	"unicode/utf8"
 )
 
 // Abbreviate Takes a string and returns the acronym of all words
 func Abbreviate(s string) string {
-	f := func(c rune) bool {
-		return !unicode.IsLetter(c) && c != '\''
-	}
 	var acronym strings.Builder
-	for _, element := range strings.FieldsFunc(s, f) {
-		r, _ := utf8.DecodeRuneInString(element)
-		acronym.WriteString(strings.ToUpper(string(r)))
+	addLetter := true
+	for _, r := range s {
+		if unicode.IsLetter(r) || r == '\'' {
+			if addLetter {
+				acronym.WriteString(strings.ToUpper(string(r)))
+			}
+			addLetter = false
+		} else {
+			addLetter = true
+		}
 	}
 	return acronym.String()
 }
