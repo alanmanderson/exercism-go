80bce7fcf406cbb47fb6eebda8a6c0949028ba5a
Alan Anderson <alan.anderson@udacity.com>
2019-05-27 09:04:03 -0700

    Update after CR

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -11,14 +11,14 @@ func Abbreviate(s string) string {
 	var acronym strings.Builder
 	addLetter := true
 	for _, r := range s {
-		if unicode.IsLetter(r) || r == '\'' {
-			if addLetter {
-				acronym.WriteString(strings.ToUpper(string(r)))
-			}
-			addLetter = false
-		} else {
+		if !unicode.IsLetter(r) && r != '\'' {
 			addLetter = true
+			continue
 		}
+		if addLetter {
+			acronym.WriteString(strings.ToUpper(string(r)))
+		}
+		addLetter = false
 	}
 	return acronym.String()
 }
