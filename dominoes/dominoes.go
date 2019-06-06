026b19ea75d1498f68cb26b8fd67ce50081a581b
Alan Anderson <alan.anderson@udacity.com>
2019-06-05 23:55:26 -0700

    Initial Checkin for Dominoes

---
+++ dominoes/dominoes.go
@@ -0,0 +1,9 @@
+package dominoes
+
+// Domino contains a top and a bottom number
+type Domino [2]int
+
+// MakeChain determines if a valid chain can be made
+func MakeChain(stones []Domino) ([]Domino, bool) {
+	return nil, true
+}
