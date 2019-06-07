d038d46a1f103e456ebe225f67b9099d60c4aa7a
Alan Anderson <alan.anderson@udacity.com>
2019-06-06 23:23:15 -0700

    WIP dominoes.go

--- dominoes/dominoes.go
+++ dominoes/dominoes.go
@@ -1,9 +1,22 @@
 package dominoes
 
+import "fmt"
+
 // Domino contains a top and a bottom number
 type Domino [2]int
 
 // MakeChain determines if a valid chain can be made
 func MakeChain(stones []Domino) ([]Domino, bool) {
+	stack := make([]Domino, 0)
+	if len(stones) == 0 {
+		return nil, true
+	}
+	stack = append(stack, stones[0])
+	stack = append(stack, flipDomino(stones[0]))
+	fmt.Printf("%v", stack)
 	return nil, true
+}
+
+func flipDomino(stone Domino) (out Domino) {
+	return Domino{stone[1], stone[0]}
 }
