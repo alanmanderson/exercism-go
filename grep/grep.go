7e9168a879d0d80f51f8ff5d28ba496cfc75b13d
Alan Anderson <alan.anderson@udacity.com>
2019-08-12 17:06:34 -0700

    Add Search function scaffold WIP

---
+++ grep/grep.go
@@ -0,0 +1,6 @@
+package grep
+
+// Search takes a pattern, flags, and a list of files and returns a list of lines that match that pattern
+func Search(pattern string, flags []string, files []string) []string {
+	return []string{}
+}
