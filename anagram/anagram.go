21aeb1f11d8449455b838ad41cf49c5361776149
Alan Anderson <alan.anderson@udacity.com>
2019-09-04 16:04:12 -0700

    Complete anagram exercise

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,5 +1,10 @@
 package anagram
 
+import (
+	"strings"
+	"unicode"
+)
+
 // Detect stuff
 func Detect(in string, anagrams []string) []string {
 	out := make([]string, 0)
@@ -12,8 +17,19 @@ func Detect(in string, anagrams []string) []string {
 }
 
 func areAnagrams(in1 string, in2 string) bool {
-	if len(in1) != len(in2) {
+	if len(in1) != len(in2) || strings.EqualFold(in1, in2) {
 		return false
+	}
+	dict := make(map[rune]int)
+	for _, r := range in1 {
+		dict[unicode.ToLower(r)]++
+	}
+	for _, r := range in2 {
+		r = unicode.ToLower(r)
+		dict[r]--
+		if dict[r] < 0 {
+			return false
+		}
 	}
 	return true
 }
