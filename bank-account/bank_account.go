0a077fa68dcdad4ec44e4baa478f76e7814431e3
Alan Anderson <alan.anderson@udacity.com>
2019-09-09 22:37:58 -0700

    Finish bank account exercise without concurrency WIP

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -2,23 +2,41 @@ package account
 
 // Act represents a bank account
 type Act struct {
-	balance float32
+	balance int64
 	closed  bool
 }
 
 //Open simulates opening a bank account
-func Open(amt float32) *Act {
+func Open(amt int64) *Act {
+	if amt < 0 {
+		return nil
+	}
 	var a = Act{amt, false}
 	return &a
 }
 
 //Balance returns the balance of the account
-func (a Act) Balance() (float32, bool) {
+func (a Act) Balance() (int64, bool) {
+	if a.closed {
+		return 0, false
+	}
 	return a.balance, true
 }
 
 //Close closes an account
-func (a Act) Close() (float32, bool) {
+func (a *Act) Close() (int64, bool) {
+	if a.closed {
+		return 0, false
+	}
 	a.closed = true
 	return a.balance, a.closed
+}
+
+//Deposit puts money into the account
+func (a *Act) Deposit(amt int64) (int64, bool) {
+	if a.closed || a.balance+amt < 0 {
+		return 0, false
+	}
+	a.balance += amt
+	return a.balance, true
 }
