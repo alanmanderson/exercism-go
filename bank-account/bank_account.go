39fb068ea18b025e42330087888ac7d1dddf1d4c
Alan Anderson <alan.anderson@udacity.com>
2019-09-10 23:25:10 -0700

    First attempt at concurrency - WIP

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -2,8 +2,8 @@ package account
 
 // Act represents a bank account
 type Act struct {
-	balance int64
-	closed  bool
+	balance chan int64
+	closed  chan bool
 }
 
 //Open simulates opening a bank account
@@ -11,32 +11,35 @@ func Open(amt int64) *Act {
 	if amt < 0 {
 		return nil
 	}
-	var a = Act{amt, false}
+	var a = Act{make(chan int64), make(chan bool)}
+	a.balance <- amt
+	a.closed <- false
 	return &a
 }
 
 //Balance returns the balance of the account
 func (a Act) Balance() (int64, bool) {
-	if a.closed {
+	if <-a.closed {
 		return 0, false
 	}
-	return a.balance, true
+	return <-a.balance, <-a.closed
 }
 
 //Close closes an account
 func (a *Act) Close() (int64, bool) {
-	if a.closed {
+	if <-a.closed {
 		return 0, false
 	}
-	a.closed = true
-	return a.balance, a.closed
+	a.closed <- true
+	return <-a.balance, <-a.closed
 }
 
 //Deposit puts money into the account
 func (a *Act) Deposit(amt int64) (int64, bool) {
-	if a.closed || a.balance+amt < 0 {
+	balance := <-a.balance - amt
+	if <-a.closed || balance < 0 {
 		return 0, false
 	}
-	a.balance += amt
-	return a.balance, true
+	a.balance <- balance
+	return <-a.balance, true
 }
