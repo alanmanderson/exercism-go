33fab576ce151da13a124f66131013daf68ff735
Alan Anderson <alan.anderson@udacity.com>
2019-09-21 13:47:07 -0700

    Add beginning of rectangles solution WIP

--- rectangles/rectangles.go
+++ rectangles/rectangles.go
@@ -1,6 +1,21 @@
 package rectangles
 
+import "fmt"
+
+type coordinate [2]int
+
 //Count returns the number of rectangles in an ASCII diagram
-func Count(ascii []string) int {
+func Count(diagram []string) int {
+	coordinates := make([]coordinate, 0)
+	for y, row := range diagram {
+		for x, r := range row {
+			if r == '+' {
+				coordinates = append(coordinates, coordinate{x, y})
+			}
+		}
+	}
+	for _, c := range coordinates {
+		fmt.Printf("%v", c)
+	}
 	return 0
 }
