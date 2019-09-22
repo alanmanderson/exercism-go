f79f85e68f16e3a4444c838f437a56e985a0712f
Alan Anderson <alan.anderson@udacity.com>
2019-09-22 14:01:59 -0700

    Add getCoordinatesOnAxis to rectangles WIP

--- rectangles/rectangles.go
+++ rectangles/rectangles.go
@@ -6,6 +6,14 @@ type coordinate [2]int
 
 //Count returns the number of rectangles in an ASCII diagram
 func Count(diagram []string) int {
+	coordinates := getCorners(diagram)
+	for _, c := range coordinates {
+		fmt.Printf("%v", c)
+	}
+	return 0
+}
+
+func getCorners(diagram []string) []coordinate {
 	coordinates := make([]coordinate, 0)
 	for y, row := range diagram {
 		for x, r := range row {
@@ -14,8 +22,23 @@ func Count(diagram []string) int {
 			}
 		}
 	}
-	for _, c := range coordinates {
-		fmt.Printf("%v", c)
+	return coordinates
+}
+
+func getCoordinatesOnAxis(coordinates []coordinate, value int, axis rune) (out []coordinate) {
+	var idx int
+	switch axis {
+	case 'x':
+		idx = 0
+	case 'y':
+		idx = 1
+	default:
+		panic("axis must be x or y")
 	}
-	return 0
+	for _, coordinate := range coordinates {
+		if coordinate[idx] == value {
+			out = append(out, coordinate)
+		}
+	}
+	return
 }
