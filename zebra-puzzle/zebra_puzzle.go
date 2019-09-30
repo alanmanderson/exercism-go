b500f5d169729ea61c4d8f58aaa089e1e82e62eb
Alan Anderson <alan.anderson@udacity.com>
2019-09-30 03:35:46 -0700

    Add more zebra-puzzle scaffold WIP

--- zebra-puzzle/zebra_puzzle.go
+++ zebra-puzzle/zebra_puzzle.go
@@ -6,7 +6,32 @@ type Solution struct {
 	OwnsZebra   string
 }
 
+type house struct {
+	order      int
+	inhabitant string
+	color      string
+	pet        string
+	beverage   string
+	cigarette  string
+}
+
 //SolvePuzzle stuff
 func SolvePuzzle() Solution {
-	return Solution{}
+	houses := [5]house{
+		house{1, "Englishman", "red", "", "", ""},
+		house{2, "", "", "", "", ""},
+		house{3, "", "", "", "", ""},
+		house{4, "", "", "", "", ""},
+		house{5, "", "", "", "", ""},
+	}
+	solution := Solution{"", ""}
+	for _, house := range houses {
+		if house.beverage == "water" {
+			solution.DrinksWater = house.inhabitant
+		}
+		if house.pet == "zebra" {
+			solution.OwnsZebra = house.inhabitant
+		}
+	}
+	return solution
 }
