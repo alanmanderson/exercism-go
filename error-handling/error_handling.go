f56ef4f8c7b9f65566307a2c9f50c48e68259aa1
Alan Anderson <alanmanderson@gmail.com>
2024-08-30 18:22:48 -0700

    Adding error-handling project

--- error-handling/error_handling.go
+++ error-handling/error_handling.go
@@ -1,8 +1,33 @@
 package erratum
 
-import "errors"
+import (
+    "errors"
+    "fmt"
+    )
 
 //Use stuff
 func Use(o ResourceOpener, input string) error {
-	return errors.New("error")
+    for {
+        r, err := o()
+        if err != nil {
+            switch err.(type) {
+                case TransientError:
+                    continue
+                default:
+                    fmt.Println("default")
+                    return errors.New("too awesome")
+            }
+            return err
+        }
+        r.Frob(input)
+        
+        defer func(r Resource) {
+            if x := recover(); x != nil {
+                r.Defrob(input)
+            }
+            r.Defrob(input)
+        }(r)
+        break;
+    }
+	return nil
 }
